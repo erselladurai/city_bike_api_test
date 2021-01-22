df8aa629fc9f667d7d53c80ecb42cb022f571a83
TOOLS\r.selladurai <Rajan.selladurai@boots.co.uk>
2021-01-22 10:41:50 +0000

    City bike Api test initial version

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,36 +1,20 @@
 package runner;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = {"src/test/resources/features/"},
-        glue = {"boots.uk.dps"},
+        features = {"src/test/resources/features"},
+        glue = {"steps"},
+        tags = {"@city_network_test"},
         strict = true
 )
-
 public class Runner extends AbstractTestNGCucumberTests {
 
-    private static Integer count = 0;
-
-
-    @DataProvider(parallel = true)
     @Override
+    @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
-
-    @Before
-    public void beforeTest(Scenario scenario) {
-        System.out.println("Currently executing -> " + ++count + "." + scenario.getName());
-    }
-
-    @After
-    public void tearDown(Scenario scenario) {
-    }
 }
-
